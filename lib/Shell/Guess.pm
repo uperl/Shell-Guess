567a1e1fef649d0fe4e159dced2ecb1b904bc7b9
Graham Ollis <perl@wdlabs.com>
2012-10-09 12:02:06 -0400

    initial implementation (unix only)

--- lib/Shell/Guess.pm
+++ lib/Shell/Guess.pm
@@ -2,8 +2,91 @@ package Shell::Guess;
 
 use strict;
 use warnings;
-use v5.10;
+use File::Spec;
 
+# ABSTRACT: make an educated guess about the shell in use
 # VERSION
+
+sub running_shell
+{
+  if($^O eq 'MSWin32')
+  {
+    die 'FIXME';
+  }
+  
+  if($^O eq 'VMS')
+  {
+    die 'FIXME';
+  }
+
+  my $shell = eval {
+    open my $fh, '<', File::Spec->catfile('', 'proc', getppid, 'cmdline');
+    my $command_line = <$fh>;
+    close $fh;
+    $command_line =~ s/\0$//;
+    _unixy_shells($command_line);
+  };
+  $shell || __PACKAGE__->login_shell;
+}
+
+sub login_shell
+{
+  shift; # class ignored
+  my $shell;
+  
+  if($^O eq 'MSWin32')
+  {
+    die 'FIXME';
+  }
+  
+  if($^O eq 'VMS')
+  {
+    die 'FIXME';
+  }
+  
+  eval {
+    my $pw_shell = (getpwnam(shift||$ENV{USER}||$ENV{USERNAME}||$ENV{LOGNAME}))[-1];
+    $shell = _unixy_shells($pw_shell);
+    $shell = _unixy_shells(readlink $pw_shell) if !defined($shell) && -l $pw_shell;
+  };
+  
+  $shell = __PACKAGE__->bourne_shell unless defined $shell;
+  
+  return $shell;
+}
+
+sub cmd_shell     { bless { cmd => 1, win32 => 1,              name => 'cmd'     }, __PACKAGE__ }
+sub command_shell { bless { command => 1, win32 => 1,          name => 'command' }, __PACKAGE__ }
+sub dcl_shell     { bless { dcl => 1, vms => 1,                name => 'dcl'     }, __PACKAGE__ }
+sub bash_shell    { bless { bash => 1, bourne => 1, unix => 1, name => 'bash'    }, __PACKAGE__ }
+sub bourne_shell  { bless { bourne => 1, unix => 1,            name => 'bourne'  }, __PACKAGE__ }
+sub korn_shell    { bless { korn => 1, bourne => 1, unix => 1, name => 'korn'    }, __PACKAGE__ }
+sub c_shell       { bless { c => 1, unix => 1,                 name => 'c'       }, __PACKAGE__ }
+sub tc_shell      { bless { c => 1, tc => 1, unix => 1,        name => 'tc'      }, __PACKAGE__ }
+
+foreach my $type (qw( cmd command dcl bash korn c win32 unix vms bourne tc ))
+{
+  eval qq{ sub is_$type { shift->{$type} || 0 } };
+  die $@ if $@;
+}
+
+sub name { shift->{name} }
+
+sub _unixy_shells
+{
+  my $shell = shift;
+  if($shell =~ /tcsh$/)
+  { return __PACKAGE__->tc_shell     }
+  elsif($shell =~ /csh$/)
+  { return __PACKAGE__->c_shell      }
+  elsif($shell =~ /ksh$/)
+  { return __PACKAGE__->korn_shell   }
+  elsif($shell =~ /bash$/)
+  { return __PACKAGE__->bash_shell   }
+  elsif($shell =~ /sh$/)
+  { return __PACKAGE__->bourne_shell }
+  else
+  { return; }
+}
 
 1;
---
+++ t/shell_c.t
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+use Test::More tests => 12;
+use Shell::Guess;
+
+my $shell = eval { Shell::Guess->c_shell };
+diag $@ if $@;
+
+isa_ok $shell, 'Shell::Guess';
+
+is eval { $shell->is_cmd     }, 0, "cmd = 0";
+diag $@ if $@;
+is eval { $shell->is_command }, 0, "command = 0";
+diag $@ if $@;
+is eval { $shell->is_dcl     }, 0, "dcl = 0";
+diag $@ if $@;
+is eval { $shell->is_bash    }, 0, "bash = 0";
+diag $@ if $@;
+is eval { $shell->is_korn    }, 0, "korn = 0";
+diag $@ if $@;
+is eval { $shell->is_c       }, 1, "c = 1";
+diag $@ if $@;
+is eval { $shell->is_win32   }, 0, "win32 = 0";
+diag $@ if $@;
+is eval { $shell->is_unix    }, 1, "unix = 1";
+diag $@ if $@;
+is eval { $shell->is_vms     }, 0, "vms = 0";
+diag $@ if $@;
+is eval { $shell->is_bourne  }, 0, "bourne = 0";
+diag $@ if $@;
+is eval { $shell->is_tc      }, 0, "tc = 0";
+diag $@ if $@;
